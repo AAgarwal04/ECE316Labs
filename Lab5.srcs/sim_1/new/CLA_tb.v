23e06c06811b01329a110184eb8987fa24d5c6e6
AAgarwal04 <121470779+AAgarwal04@users.noreply.github.com>
2023-12-17 15:00:53 -0500

    Add files via upload

---
+++ Lab5.srcs/sim_1/new/CLA_tb.v
@@ -0,0 +1,68 @@
+`timescale 1ns / 1ps
+
+module CLA_tb;
+
+reg clk;
+reg enable;
+reg [3:0] A, B;
+reg Cin;
+
+wire [4:0] Q;
+
+CLA_4bits uut(
+    .clk(clk),
+    .enable(enable),
+    .A(A),
+    .B(B),
+    .Cin(Cin),
+    .Q(Q)
+);
+
+initial
+begin
+
+clk = 0;
+enable = 1;
+Cin = 0;
+
+A = 4'b0000;
+B = 4'b0101;
+
+#10;
+
+A = 4'b0101;
+B = 4'b0111;
+
+#10
+
+Cin = 1;
+A = 4'b1000;
+B = 4'b0111;
+
+#10;
+Cin = 0;
+
+A = 4'b1001;
+B = 4'b0100;
+
+#10;
+Cin = 1;
+A = 4'b1000;
+B = 4'b1000;
+
+#10
+Cin = 1;
+A = 4'b1101;
+B = 4'b1010;
+
+#10
+Cin = 0;
+A = 4'b1110;
+B = 4'b1111;
+
+end
+
+always
+#5 clk = ~clk;
+
+endmodule
---
+++ Lab5.srcs/sources_1/new/CLA_4bits.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+
+
+module CLA_4bits(
+    input clk,
+    input enable,
+    input [3:0] A, B,
+    input Cin,
+    output [4:0] Q
+    );
+    
+    wire [3:0] G, P, S;
+    wire [4:0] C;
+    wire Cout;
+    wire [4:0] DataReg;
+    assign C[0] = Cin;
+    
+    assign G[0] = A[0] * B[0];
+    assign G[1] = A[1] * B[1];
+    assign G[2] = A[2] * B[2];
+    assign G[3] = A[3] * B[3];
+    
+    assign P[0] = A[0] ^ B[0];
+    assign P[1] = A[1] ^ B[1];
+    assign P[2] = A[2] ^ B[2];
+    assign P[3] = A[3] ^ B[3];
+    
+    assign C[1] = G[0] + (P[0]*C[0]);
+    assign C[2] = G[1] + (P[1]*G[0]) + (P[0]*C[0]*P[1]);
+    assign C[3] = G[2] + (P[2]*G[1]) + (P[2]*P[1]*G[0]) + (P[2]*P[1]*P[0]*C[0]);
+    assign C[4] = G[3] + (P[3]*G[2])+ (P[3]*P[2]*G[1]) + (P[3]*P[2]*P[1]*G[0]) + (P[3]*P[2]*P[1]*P[0]*C[0]);
+    assign Cout = C[4];
+    
+    assign S[0] = P[0] ^ C[0];
+    assign S[1] = P[1] ^ (G[0] + (P[0]*C[0]));
+    assign S[2] = P[2] ^ (G[1] + (P[1]*G[0]) + (P[0]*C[0]*P[1]));
+    assign S[3] = P[3] ^ (G[2] + (P[2]*G[1]) + (P[2]*P[1]*G[0]) + (P[2]*P[1]*P[0]*C[0]));
+    
+    assign DataReg[3:0] = S;
+    assign DataReg[4] = Cout;
+    
+    register_logic r(.clk(clk), .enable(enable), .Data(DataReg), .Q(Q));
+    
+endmodule
